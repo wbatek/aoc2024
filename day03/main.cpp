035199b3f9a2b7c74b227b875e4712903c93b8ce
wbatek <wojciech.batko02@gmail.com>
2024-12-03 11:46:03 +0100

    day 03 solution

--- day03/main.cpp
+++ day03/main.cpp
@@ -26,40 +26,33 @@ void writeResultToFile(T result, int taskNumber) {
     outFile.close();
 }
 
-int findSum(std::string line) {
-    std::regex regex_part1("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-    std::sregex_iterator it(line.begin(), line.end(), regex_part1);
+int findSum(std::string line, std::regex _regex) {
+    std::sregex_iterator it(line.begin(), line.end(), _regex);
     std::sregex_iterator end;
 
     int sum = 0;
+    bool enable = true;
     while (it != end) {
         std::smatch match = *it;
-        int first = std::stoi(match.str(1));
-        int second = std::stoi(match.str(2));
-        sum += first * second;
+        std::string mulMatch = match.str();
+        if(mulMatch == "do()") {
+            enable = true;
+        }
+        else if(mulMatch == "don't()") {
+            enable = false;
+        }
+        else {
+            if (enable) {
+                size_t startPos = mulMatch.find('(') + 1;
+                size_t endPos = mulMatch.find(')');
+                std::string nums = mulMatch.substr(startPos, endPos - startPos);
+                size_t commaPos = nums.find(',');
+                int first = std::stoi(nums.substr(0, commaPos));
+                int second = std::stoi(nums.substr(commaPos + 1));
+                sum += first * second;
+            }
+        }
         ++it;
-    }
-    return sum;
-}
-
-std::vector<std::string> divideLine(std::string line) {
-    std::vector<std::string> result;
-    std::regex pattern("(do\\(\\)(.*?)don't\\(\\))");
-    std::sregex_iterator it(line.begin(), line.end(), pattern);
-    std::sregex_iterator end;
-
-    while(it != end) {
-        std::smatch match = *it;
-        result.push_back(match.str(1));
-        ++it;
-    }
-    return result;
-}
-
-int findSumVector(std::vector<std::string> lines) {
-    int sum = 0;
-    for(auto& vec : lines) {
-        sum += findSum(vec);
     }
     return sum;
 }
@@ -74,11 +67,14 @@ int main() {
     catch (const std::ios_base::failure& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
-    int result_1 = findSum(line);
+    std::regex regex_part1("mul\\(\\d{1,3},\\d{1,3}\\)");
+    int result_1 = findSum(line, regex_part1);
     writeResultToFile(result_1, 1);
     // END PART 1
 
-    std::vector<std::string> validParts = divideLine(line);
-    int result_2 = findSumVector(validParts);
+    // PART 2
+    std::regex regex_part2("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)");
+    int result_2 = findSum(line, regex_part2);
     writeResultToFile(result_2, 2);
+    // END PART 2
 }
